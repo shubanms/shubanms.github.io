d3abf88f88679d9eebc6211ecaa7e9f9a57b2c34
Shuban <msshuban4@gmail.com>
2022-01-01 10:21:44 +0000

    Confetti added

--- app.js
+++ app.js
@@ -45,3 +45,19 @@ masks.forEach( mask => {
         ease: Power2.out
     });
 })
+
+
+const start = () => {
+    setTimeout(function() {
+        confetti.start()
+    }, 1000);
+};
+
+const stop = () => {
+    setTimeout(function() {
+        confetti.stop()
+    }, 5000);
+};
+
+start();
+stop();
